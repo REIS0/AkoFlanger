e995f9d3331ca6bd1faf438c385084f5497d6347
REIS0 <aor@cesar.school>
2021-04-05 21:25:38 -0300

    multiple delay lines

--- plugins/AkoFlanger/AkoFlanger.cpp
+++ plugins/AkoFlanger/AkoFlanger.cpp
@@ -105,16 +105,33 @@ protected:
     float *const outR = outputs[1];
     for (uint32_t i = 0; i < frames; ++i) {
       float n = lfo.get_value();
-      vL = n * (ff_bufferL.get_value(p3) - vL) + ff_bufferL.get_value(p3 - 1);
-      vR = n * (ff_bufferR.get_value(p3) - vR) + ff_bufferR.get_value(p3 - 1);
-      outL[i] = inL[i] + depth * vL - regen * fd_bufferL.get_value(p3);
-      outR[i] = inR[i] + depth * vR - regen * fd_bufferL.get_value(p3);
+      v1L = n * (ff_bufferL.get_value(p1) - v1L) + ff_bufferL.get_value(p1 - 1);
+      v1R = n * (ff_bufferR.get_value(p1) - v1R) + ff_bufferR.get_value(p1 - 1);
+      v2L = n * (ff_bufferL.get_value(p2) - v2L) + ff_bufferL.get_value(p2 - 1);
+      v2R = n * (ff_bufferR.get_value(p2) - v2R) + ff_bufferR.get_value(p2 - 1);
+      v3L = n * (ff_bufferL.get_value(p3) - v3L) + ff_bufferL.get_value(p3 - 1);
+      v3R = n * (ff_bufferR.get_value(p3) - v3L) + ff_bufferR.get_value(p3 - 1);
+      // TODO: feedback modulation
+      outL[i] = inL[i] + (depth * 0.3) * v1L + (depth * 0.4) * v2L +
+                (depth * 0.5) * v3L - regen * fd_bufferL.get_value(p3);
+      outR[i] = inR[i] + (depth * 0.3) * v1R + (depth * 0.4) * v2R +
+                (depth * 0.5) * v3R - regen * fd_bufferL.get_value(p3);
       ff_bufferL.set_value(p3, inL[i]);
       ff_bufferR.set_value(p3, inR[i]);
       fd_bufferL.set_value(p3, outL[i]);
       fd_bufferR.set_value(p3, outR[i]);
+      outL[i] = outL[i] * out_gain;
+      outR[i] = outR[i] * out_gain;
+      p1++;
+      p2++;
       p3++;
-      if (p3 >= delay3) {
+      if (p1 >= delay) {
+        p1 = 0;
+      }
+      if (p2 >= delay) {
+        p2 = 0;
+      }
+      if (p3 >= delay) {
         p3 = 0;
       }
     }
@@ -134,8 +151,12 @@ private:
   int p3;
 
   // y(n - 1) from allpass interpolation
-  float vL = 0;
-  float vR = 0;
+  float v1L = 0;
+  float v1R = 0;
+  float v2L = 0;
+  float v2R = 0;
+  float v3L = 0;
+  float v3R = 0;
 
   // PARAMS
 
