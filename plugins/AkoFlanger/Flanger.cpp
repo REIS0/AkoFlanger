c295bc20ad65c796a45802ead418774447b570f7
REIS0 <aor@cesar.school>
2021-04-25 12:03:47 -0300

    Update Flanger class

--- plugins/AkoFlanger/Flanger.cpp
+++ plugins/AkoFlanger/Flanger.cpp
@@ -2,18 +2,26 @@
 
 /* ----- PUBLIC ----- */
 
-Flanger::Flanger(int buffer_size)
-    : buffer(buffer_size), buffer_size(buffer_size) {}
+Flanger::Flanger(int delay, int max_delay)
+    : delay(delay), max_delay(max_delay), diff(max_delay - delay), buffer(max_delay) {
+  // vecbuffer.assign(max_delay, 0.0f);
+}
 
 // use external LFO
-// ? external depth ? 
-float Flanger::process(float n, float x) {
-  float y =
-      x + cubic_interpolation(n, buffer.get_value(p - 1), buffer.get_value(p),
-                              buffer.get_value(p + 1), buffer.get_value(p + 2));
+float Flanger::process(float n, float depth, float x) {
+  float y = x + depth * cubic_interpolation(n, buffer.get_value(p),
+                                            buffer.get_value(p + diff *
+                                            0.33), buffer.get_value(p + diff
+                                            * 0.66), buffer.get_value(p +
+                                            diff));
   buffer.set_value(p, x);
+  // float y = x + depth * cubic_interpolation(
+  //                           n, vecbuffer.front(), vecbuffer.at(diff * 0.33),
+  //                           vecbuffer.at(diff * 0.66), vecbuffer.at(diff));
+  // vecbuffer.erase(vecbuffer.begin());
+  // vecbuffer.push_back(x);
   p++;
-  if (p >= buffer_size) {
+  if (p >= max_delay) {
     p = 0;
   }
   return y;
@@ -25,9 +33,8 @@ float Flanger::process(float n, float x) {
 // https://www.musicdsp.org/en/latest/Other/49-cubic-interpollation.html
 float Flanger::cubic_interpolation(float finpos, float xm1, float x0, float x1,
                                    float x2) {
-  float a = (3 * (x0 - x1) - xm1 - x2) * 0.5;
-  float b = 2 * x1 + xm1 - (5 * x0 + x2) * 0.5;
-  float c = (x1 - xm1) * 0.5;
-  float y = (((a * finpos) + b) * finpos + c) * finpos + x0;
-  return y;
+  float a = (3.f * (x0 - x1) - xm1 - x2) * 0.5f;
+  float b = 2.f * x1 + xm1 - (5.f * x0 + x2) * 0.5f;
+  float c = (x1 - xm1) * 0.5f;
+  return (((a * finpos) + b) * finpos + c) * finpos + x0;
 }
--- plugins/AkoFlanger/Flanger.hpp
+++ plugins/AkoFlanger/Flanger.hpp
@@ -1,16 +1,22 @@
 #include "RingBuffer.hpp"
+#include <vector>
 
 class Flanger {
 public:
-  Flanger(int buffer_size);
-  float process(float n, float x);
+  Flanger(int delay, int max_delay);
+  float process(float n, float depth, float x);
 
 private:
   float cubic_interpolation(float finpos, float xm1, float x0, float x1,
                             float x2);
 
+  int delay;
+  int max_delay;
+  int diff;
+  // ? vectors sem precisar de uma classe separada ?
   RingBuffer buffer; // feedforward buffer
 
+  // std::vector<float> vecbuffer;
+
   int p = 0;
-  int buffer_size;
 };
